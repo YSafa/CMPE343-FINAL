f49fdf93fb9c0c1bcf2337d92ddf08e2b62297a4
Sena Coşkun <coskunsena3246@gmail.com>
2025-12-21 17:51:22 +0300

    package ayarlandı

--- src/Group29.java
+++
@@ -1,9 +0,0 @@
-
-public class Group29
-{
-    public static void main(String[] args)
-    {
-
-
-    }
-}
---
+++ src/application/Group29.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Group29 extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Group29 GreenGrocer");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
